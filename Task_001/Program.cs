61df76bcbb3929e06e903d6002800e71c9d3161c
Finaev <antonfinaev@icloud.com>
2024-07-23 17:45:51 +0300

    task1

---
+++ Task_001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задание 1 Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+// код
+class Program
+{
+    static void PrintNumbers(int M, int N)
+    {
+        if (M > N) return;
+
+        Console.Write(M + " ");
+        PrintNumbers(M + 1, N);
+    }
+
+    static void Main(string[] args)
+    {
+        int M = 1;
+        int N = 10;
+
+        PrintNumbers(M, N);
+    }
+}
