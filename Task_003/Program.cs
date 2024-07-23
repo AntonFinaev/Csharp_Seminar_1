2929f2270ffae53ec654da8a388e02d2cc8cf0db
Finaev <antonfinaev@icloud.com>
2024-07-23 17:57:16 +0300

    task3

---
+++ Task_003/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+class Program
+{
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index < 0) return;
+
+        Console.Write(array[index] + " ");
+        PrintArrayReverse(array, index - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        if (args.Length > 0)
+        {
+            array = new int[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (int.TryParse(args[i], out int result))
+                {
+                    array[i] = result;
+                }
+                else
+                {
+                    Console.WriteLine($"Error parsing element {args[i]} to an integer.");
+                    return;
+                }
+            }
+        }
+
+        PrintArrayReverse(array, array.Length - 1);
+    }
+}
+
